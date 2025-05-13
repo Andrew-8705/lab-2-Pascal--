0efa5a0f2505ce9e867399f5047452bb57745394
Andrew-8705 <abaldin05@mail.ru>
2025-05-14 00:37:41 +0300

    more tests for Lexer

--- LAB2_PASCAL--/test/test_lexer.cpp
+++ LAB2_PASCAL--/test/test_lexer.cpp
@@ -8,14 +8,18 @@
 using namespace std;
 
 bool CompareTokens(const vector<Token>& expected, const vector<Token>& actual) {
-    cout << expected.size() << ' ' << actual.size() << '\n';
+    //cout << expected.size() << ' ' << actual.size() << '\n';
     if (expected.size() != actual.size()) return false;
     for (size_t i = 0; i < expected.size(); ++i) {
         if (expected[i].type != actual[i].type ||
             expected[i].value != actual[i].value ||
             expected[i].line != actual[i].line ||
             expected[i].column != actual[i].column) {
-            cout << "IIIII: " << i << '\n';
+            //cout << "IIIII: " << i << '\n';
+            //cout << (int)expected[i].type << ' ' << (int)actual[i].type << '\n';
+            //cout << expected[i].value << ' ' << actual[i].value << '\n';
+            //cout << expected[i].line << ' ' << actual[i].line << '\n';
+            //cout << expected[i].column << ' ' << actual[i].column << '\n';
             return false;
         }
     }
@@ -119,7 +123,7 @@ TEST(LexerTest, can_tokenize_comparison_operators) {
         {TokenType::GREATER_OR_EQUAL, ">=", 1, 10},
         {TokenType::LESS_OR_EQUAL, "<=", 1, 13}
     };
-    ASSERT_TRUE(CompareTokens(expectedTokens, lexer.tokenize()));
+    EXPECT_TRUE(CompareTokens(expectedTokens, lexer.tokenize()));
 }
 
 TEST(LexerTest, can_tokenize_separators) {
@@ -133,3 +137,65 @@ TEST(LexerTest, can_tokenize_separators) {
     };
     EXPECT_TRUE(CompareTokens(expectedTokens, lexer.tokenize()));
 }
+
+TEST(LexerTest, can_ignore_whitespace) {
+    string source = "  program\n\tvar \n begin";
+    Lexer lexer(source);
+    vector<Token> expectedTokens = {
+        {TokenType::KEYWORD_PROGRAM, "program", 1, 3},
+        {TokenType::KEYWORD_VAR, "var", 2, 2},
+        {TokenType::KEYWORD_BEGIN, "begin", 3, 2}
+    };
+    EXPECT_TRUE(CompareTokens(expectedTokens, lexer.tokenize()));
+}
+
+TEST(LexerTest, can_tokenize_end_of_program) {
+    string source = "end.";
+    Lexer lexer(source);
+    vector<Token> expectedTokens1 = {
+        {TokenType::END_OF_PROGRAM, "end.", 1, 1}
+    };
+    EXPECT_TRUE(CompareTokens(expectedTokens1, lexer.tokenize()));
+
+    source = "end .";
+    lexer = Lexer(source);
+    vector<Token> expectedTokens2 = {
+        {TokenType::KEYWORD_END, "end", 1, 1},
+        {TokenType::UNKNOWN, ".", 1, 5}
+    };
+    EXPECT_TRUE(CompareTokens(expectedTokens2, lexer.tokenize()));
+}
+
+TEST(LexerTest, can_tokenize_mixed_tokens) {
+    string source = "program myProgram; var counter: integer; begin Write(\"hello\"); end.";
+    Lexer lexer(source);
+    vector<Token> expectedTokens = {
+        {TokenType::KEYWORD_PROGRAM, "program", 1, 1},
+        {TokenType::IDENTIFIER, "myProgram", 1, 9},
+        {TokenType::SEMICOLON, ";", 1, 18},
+        {TokenType::KEYWORD_VAR, "var", 1, 20},
+        {TokenType::IDENTIFIER, "counter", 1, 24},
+        {TokenType::COLON, ":", 1, 31},
+        {TokenType::KEYWORD_INTEGER, "integer", 1, 33},
+        {TokenType::SEMICOLON, ";", 1, 40},
+        {TokenType::KEYWORD_BEGIN, "begin", 1, 42},
+        {TokenType::KEYWORD_WRITE, "Write", 1, 48},
+        {TokenType::LEFT_PAREN, "(", 1, 53},
+        {TokenType::STRING_LITERAL, "hello", 1, 54},
+        {TokenType::RIGHT_PAREN, ")", 1, 61},
+        {TokenType::SEMICOLON, ";", 1, 62},
+        {TokenType::END_OF_PROGRAM, "end.", 1, 64}
+    };
+    EXPECT_TRUE(CompareTokens(expectedTokens, lexer.tokenize()));
+}
+
+TEST(LexerTest, throws_error_for_unterminated_string) {
+    Lexer lexer("\"hello");
+    EXPECT_THROW(lexer.tokenize(), runtime_error);
+}
+
+TEST(LexerTest, throws_error_for_unknown_token) {
+    Lexer lexer("#");
+    ASSERT_THROW(lexer.tokenize(), runtime_error);
+}
+
